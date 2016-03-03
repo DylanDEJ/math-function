5ca122230780c53b2779077e8f4e8c15fa13a8d5
DylanDEJ <1017765@idcollege.nl>
2016-03-03 14:21:54 +0100

    math

---
+++ calculate.php
@@ -0,0 +1,22 @@
+<?php
+
+function math($getal1,$getal2,$type){
+if($type == "+"){
+ echo $getal1 + $getal2;
+}
+ else if ($type == "-"){
+  echo $getal1 + $getal2;
+ }
+ else if($type == "/"){
+
+ }
+}
+ math(1, 2, '+');
+
+?>
+/**
+ * Created by PhpStorm.
+ * User: dylan_000
+ * Date: 2-3-2016
+ * Time: 09:14
+ */
